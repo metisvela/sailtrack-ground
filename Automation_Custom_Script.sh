426eb5ff14c8a1e5abeddc82e4554d3d61f8193a
Magform <nicolas25.02.2003.nf@gmail.com>
2023-11-09 12:05:52 +0100

    Updated dietipi-service setup

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -21,11 +21,14 @@ G_CONFIG_INJECT "+ sailtrack-lora2mqtt" "+ sailtrack-lora2mqtt" /boot/dietpi/.di
 G_CONFIG_INJECT "+ sailtrack-timesync" "+ sailtrack-timesync" /boot/dietpi/.dietpi-services_include_exclude
 G_CONFIG_INJECT "+ sailtrack-tileserver" "+ sailtrack-tileserver" /boot/dietpi/.dietpi-services_include_exclude
 G_CONFIG_INJECT "+ sailtrack-kiosk" "+ sailtrack-kiosk" /boot/dietpi/.dietpi-services_include_exclude
-G_EXEC /boot/dietpi/dietpi-services dietpi_controlled telegraf
-G_EXEC /boot/dietpi/dietpi-services dietpi_controlled sailtrack-lora2mqtt
-G_EXEC /boot/dietpi/dietpi-services dietpi_controlled sailtrack-timesync
-G_EXEC /boot/dietpi/dietpi-services dietpi_controlled sailtrack-tileserver
-G_EXEC /boot/dietpi/dietpi-services dietpi_controlled sailtrack-kiosk
+G_EXEC /boot/dietpi/dietpi-services enable telegraf
+G_EXEC /boot/dietpi/dietpi-services start telegraf
+G_EXEC /boot/dietpi/dietpi-services enable sailtrack-lora2mqtt
+G_EXEC /boot/dietpi/dietpi-services start sailtrack-lora2mqtt
+G_EXEC /boot/dietpi/dietpi-services enable sailtrack-timesync
+G_EXEC /boot/dietpi/dietpi-services start sailtrack-timesync
+G_EXEC /boot/dietpi/dietpi-services enable sailtrack-tileserver
+G_EXEC /boot/dietpi/dietpi-services start sailtrack-kiosk
 
 # Configure DietPi Banner
 G_EXEC touch /boot/dietpi/.dietpi-banner
